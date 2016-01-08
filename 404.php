08c68c0b6469cb22cd484aab8af6c2983a3bab54
Look <l00k4n7h3sky@gmail.com>
2016-01-08 15:21:52 -0300

    Update 404.php

--- 404.php
+++ 404.php
@@ -30,10 +30,10 @@ header($_SERVER["SERVER_PROTOCOL"]." 404 Not Found");
     <div class="container">
       <div class="jumbotron">
 	    <h1>404</h1>
-		<h4>Sorry, we couldn't find that page.</h4>
+		<h4>Desculpe, mas a página especificada não foi encontrada.</h4>
 		<div class="btn-group" role="group" aria-label="...">
-		  <a href="#" class="btn btn-primary btn-lg" onclick="window.history.back()">Go back</a>
-		  <a href="/" class="btn btn-success btn-lg">Homepage</a>
+		  <a href="#" class="btn btn-primary btn-lg" onclick="window.history.back()">Voltar</a>
+		  <a href="/" class="btn btn-success btn-lg">Página inicial</a>
 	    </div>
 	  </div>
 	</div>
